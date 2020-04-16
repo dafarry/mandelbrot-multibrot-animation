d8b0cdcfd26f224862a552215536451e0041753e
dafarry <32924849+dafarry@users.noreply.github.com>
2020-04-16 08:42:42 +0100

    Add files via upload

--- multibrot-make-gif.py
+++ multibrot-make-gif.py
@@ -3,20 +3,20 @@ import numpy as np
 from PIL import Image, ImageDraw, ImageFont # Pillow fork
 import os
 
-ttf = "DejaVuSansMono-Bold.ttf"             # An installed truetype font
-w, h = 660, 500                             # height, width
+ttf = "DejaVuSansMono-Bold.ttf"             # A monospace truetype font
+w, h = 660, 500                             # width, height
 font_a = ImageFont.truetype(ttf, h // 20)   # font size
 xc, s = -0.35, 1.75                         # x-center, and scaling
 y, x = np.ogrid[-s * h/w: 1e-4: 1j * h/2, xc - s: xc + s: 1j * w]
-c = x + 1j*y                                # complex plane
-os.makedirs("video", exist_ok=True)         # dir for images
+c = x + 1j*y                                # mandelbrot const complex array
+os.makedirs("video", exist_ok=True)         # dir for image sequence
 blues = [(v**4, v**2.5, v) for v in np.linspace(0, 1, 72)]
 sepias = [(v, v**1.5, v**3) for v in np.linspace(1, 0, 12) for _ in range(6)]
 rgbcolors = np.uint8(np.array(blues + sepias) * 255)
-t = 200                                     # total frames
+t = 250                                     # total frames
 for f in range(t):
-    #asymptotically rising power value over range 1.05 - 10000
-    n = 2 + 2 * f / (1e-9 + t - f - 1 + (2 * t - 2) / (1e4 - 1.05))
+    #asymptotically rising power value over range 2 - 10000
+    n = 2 + 2 * f / (1e-9 + t - f - 1 + (2 * t - 2) / (1e4 - 2))
     # slow and stop at each whole number by adding sine wave
     p = ((n * 2 * np.pi) + np.sin((n * 2 - 1) * np.pi)) / (2 * np.pi)
     imax = 10 + int((250 / ( p - 0.75)))    # max iterations
@@ -33,8 +33,8 @@ for f in range(t):
     # calc for continuous gradient of background
     zlog = (its - np.log(abs(np.log(abs(z)))) / np.log (p)) * np.log (p)
     # tweak gradient for good contrast at all power values
-    backgnd = np.log1p(np.fmax(0, zlog * 2.6)) * (0.15 + 0.01 * np.log(p))
-    np.putmask(backgnd, mask, np.zeros_like(mask))
+    backgnd = np.log1p(np.fmax(0, zlog * 2.6)) * (0.15 + np.log(p) / 94)
+    np.putmask(backgnd, mask, np.zeros_like(mask))  # blackout the set
     tones = np.uint8(np.clip(backgnd, 0, 1) * 143)
     fullheight = np.vstack((tones, np.flipud(tones)[1:],tones[0]))
     image = Image.fromarray(rgbcolors[fullheight], mode="RGB")
